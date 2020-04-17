435c0531b81e6b79ff24e998178469c024e5289b
Harionotph45 <63421146+Harionotph45@users.noreply.github.com>
2020-04-17 15:20:32 -0400

    Final Change May Be

--- data_analysis.sh
+++ data_analysis.sh
@@ -1,3 +1,4 @@
+#The True First Line is Here
 #The First Line should be like this, but also like this
 #!/bin/bash
 #This will contain my new analytical method which does super awesome cool things for fish! Fish are my favortie, specifically rockfish
@@ -8,5 +9,7 @@
 #FUnction 2
 #Function 3
 #Function 4
+#Function 5 (Final)
 
 #This is the new things
+#The Last Line Should be like this
