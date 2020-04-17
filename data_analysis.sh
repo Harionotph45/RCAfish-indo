56e7d5815d5fd1d55f697063d4a14e89ef477b7d
madmolecularman <gree9242@uri.edu>
2020-04-17 15:11:38 -0400

    Added my favorite fish

--- data_analysis.sh
+++ data_analysis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#This will contain my new analytical method which does super awesome cool things for fish!
+#This will contain my new analytical method which does super awesome cool things for fish! Fish are my favortie, specifically rockfish
 #Changes3
 #Changes4
 
